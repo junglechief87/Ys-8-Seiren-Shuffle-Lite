0ad2a166866428105452bb40c50adc6a3be75570
jungleChief87 <Chuch787@gmail.com>
2026-04-25 21:50:19 -0500

    fixing references

--- Seiren Shuffle Lite/setup.py
+++ Seiren Shuffle Lite/setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='Ys_8_Rando', version='0.9', packages=find_packages())
+setup(name='Ys_8_Lite_AP_Patcher', version='0.9', packages=find_packages())
