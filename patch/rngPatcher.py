266ec1432ebfa4f7085f5860acd51ab53d759e94
jungleChief87 <Chuch787@gmail.com>
2026-05-21 18:12:37 -0500

    some reorg

--- patch/rngPatcher.py
+++ patch/rngPatcher.py
@@ -1,9 +1,9 @@
 import random
 import os
 from shared.functions import *  
-from patch.crew import CREW_FLAGS
-from patch.bossCue import BOSS_CUE
-from patch.fscWarp import FSC_WARP
+from script_constants.crew import CREW_FLAGS
+from script_constants.bossCue import BOSS_CUE
+from script_constants.fscWarp import FSC_WARP
 from patch.gameStartFunctions import *
 from patch.chestPatcher import *
 from patch.miscPatches import randomizeOctoBosses, newExpMult
